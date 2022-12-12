0384e5f78e67aa4c940fa2d0161466d487f9f51b
Ian Dao <ibdao921@gmail.com>
2022-12-12 10:22:43 -0800

    playing with css

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -1,9 +1,12 @@
 import React from "react";
+import './Connect.css'
 
 function Connect(){
     return(
         <div className="Connect col-8 col-12-medium">
-            <p>Connect with me page</p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/ian-dao-539523117/"> https://www.linkedin.com/in/ian-dao </a></p>
+            <p>Email:<a className="email" href="mailto:ibdao921@gmail.com"> ibdao921@gmail.com </a> </p>
+            <p>Resume: <a>Click here to download</a></p>
         </div>
     )
 }
